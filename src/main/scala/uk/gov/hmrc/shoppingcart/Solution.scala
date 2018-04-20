e69bf80095fcb4eaa4d8c49f5ff5d0c93d82a208
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-04-20 08:11:44 +0100

    rename Item to ShoppingItem

--- src/main/scala/uk/gov/hmrc/shoppingcart/Solution.scala
+++ src/main/scala/uk/gov/hmrc/shoppingcart/Solution.scala
@@ -10,7 +10,7 @@ object Solution {
   val APPLECOST = BigDecimal("0.60")
   type Amount = BigDecimal
 
-  sealed trait Item {
+  sealed trait ShoppingItem {
     def name: String
 
     def cost: Amount
@@ -18,12 +18,12 @@ object Solution {
     def quantityOffer: QuantityOffer
   }
 
-  final case class AppleItem(name: String, cost: Amount, quantityOffer: QuantityOffer) extends Item
+  final case class AppleItem(name: String, cost: Amount, quantityOffer: QuantityOffer) extends ShoppingItem
 
-  final case class OrangeItem(name: String, cost: Amount, quantityOffer: QuantityOffer) extends Item
+  final case class OrangeItem(name: String, cost: Amount, quantityOffer: QuantityOffer) extends ShoppingItem
 
-  object Item {
-    def itemFromString(str: String): Option[Item] =
+  object ShoppingItem {
+    def itemFromString(str: String): Option[ShoppingItem] =
       str match {
         case "Apple" => Some(AppleItem("Apple", APPLECOST, BuyOneGetOneFree))
         case "Orange" => Some(OrangeItem("Orange", ORANGECOST, ThreeForThePriceOfTwo))
@@ -37,7 +37,7 @@ object Solution {
 
   object Step1ShoppingService extends ShoppingService {
     override def checkout(items: List[String]): Option[Amount] =
-      Traverse[List].traverse(items)(Item.itemFromString).map(_.map(_.cost).sum)
+      Traverse[List].traverse(items)(ShoppingItem.itemFromString).map(_.map(_.cost).sum)
   }
 
   // Step  2
@@ -67,18 +67,18 @@ object Solution {
   }
 
   trait QuantityOfferCalculation {
-    def quantityOffer(items: List[Item]): List[Item]
+    def quantityOffer(shoppingItems: List[ShoppingItem]): List[ShoppingItem]
   }
 
   object Step2ShoppingService extends ShoppingService with QuantityOfferCalculation {
     override def checkout(items: List[String]): Option[Amount] =
       for {
-        itemObjects <- Traverse[List].traverse(items)(Item.itemFromString)
+        itemObjects <- Traverse[List].traverse(items)(ShoppingItem.itemFromString)
       } yield
         quantityOffer(itemObjects).map(_.cost).sum
 
 
-    override def quantityOffer(items: List[Item]): List[Item] =
+    override def quantityOffer(items: List[ShoppingItem]): List[ShoppingItem] =
       items.groupBy(identity).flatMap(kv => {
         val quotient = kv._2.size / kv._1.quantityOffer.buy
         val remainder = kv._2.size % kv._1.quantityOffer.buy
